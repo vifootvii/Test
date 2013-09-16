561913b5b4c07ab0fa8222c2475b46fdaf3bd050
Doug Andrew <vifootvii@gmail.com>
2013-09-16 17:41:34 -0600

    asdfasdf @[f:#;wl:#hrs;s:10]
    ==========
    The first key/value can be an 'f' or a 'd'
    f:# = specifies the commit is for a FEATURE with the corresponding ID
    d:# = specifies the commit is for a DEFECT with the corresponding ID
    
    wl:#hrs = specifies the ammount of work to log against the item
              valid time units are:
                      MINUTES: m,min,mm
                        HOURS: h,hr,hrs,hh
                         DAYS: d,dd,days,day
                        WEEKS: w,wks,week,weeks
                 STORY_POINTS: s,sp,story points
    
    s:# = specifies the ID for the  workflow step to move the item to
          valid workflow steps include:
                  New Request: 10
                     Rejected: 20
                     Approved: 30
                  Development: 40
              Ready for Build: 50
                   QA Testing: 60
              Ready to Deploy: 70
                    Completed: 80

--- test/test/PLAppDelegate.m
+++ test/test/PLAppDelegate.m
@@ -13,7 +13,7 @@
 @implementation PLAppDelegate
 
 //test commentfghfghf
-//another testghjgsdfsdfsdfsdfkgsdfsdfsdfsfsdfsdfsdsafwerwersdfsdfasdfasdf sadfsdf
+//another testghjgsdfsdfsdfsdfkgsdfsdfsdfsfsdfsdfsdsafwerwersdfsdfasdfasdf sadfsdfasdf
 
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize managedObjectModel = _managedObjectModel;
