31ad0edcbb605fc3dbe9413caa1270e10c99f1a4
Doug Andrew <vifootvii@gmail.com>
2013-09-16 09:23:37 -0600

    @[f:1;wl:4hrs;s:10]

--- test/test/PLAppDelegate.m
+++ test/test/PLAppDelegate.m
@@ -13,7 +13,7 @@
 @implementation PLAppDelegate
 
 //test commentfghfghf
-//another testghjgsdfsdfsdfsdfkgsdfsdfsdfsfsdfsdfsdsafwerwersdfsdf
+//another testghjgsdfsdfsdfsdfkgsdfsdfsdfsfsdfsdfsdsafwerwersdfsdfasdfasdf 
 
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize managedObjectModel = _managedObjectModel;
