d4efa49c220ba529d62456aebbef0a3c424e3e4e
Doug Andrew <vifootvii@gmail.com>
2013-09-16 20:47:40 -0600

    This is a test @[f:#;wl:#hrs;s:10]

--- test/test/PLAppDelegate.m
+++ test/test/PLAppDelegate.m
@@ -13,7 +13,7 @@
 @implementation PLAppDelegate
 
 //test commentfghfghf
-//another testghjgsdfsdfsdfsdfkgsdfsdfsdfsfsdfsdfsdsafwerwersdfsdfasdfasdf sadfsdfasdfsdfasdfasfasdfasdf
+//another ZXC
 
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize managedObjectModel = _managedObjectModel;
